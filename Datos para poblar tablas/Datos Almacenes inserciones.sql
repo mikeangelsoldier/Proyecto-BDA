1376c0f858001fd6ed116ea6c9b30d55e7a41b13
mikeangelsoldier <mikeangelsoldier1@gmail.com>
2018-11-08 22:13:06 -0600

    Se agregaron en  nuevos scripts los datos para poblar algunas tablas que estaban antes en el script general, ahora cada script tiene datos para llenar cada tabla por separado

---
+++ Datos para poblar tablas/Datos Almacenes inserciones.sql
@@ -0,0 +1,15 @@
+
+-----------------------------------ALMACEN_EMPRESA-------------------------------------------------------------    SELECT * FROM ALMACEN_EMPRESA
+
+USE Nissan
+GO
+
+INSERT INTO ALMACEN_EMPRESA(ID_ALMACEN_EMPRESA,DESCRIPCION,ESTADO,CIUDAD,CP,COLONIA,CALLE,NUM_INT) 
+VALUES
+('ALM0001','ALMACEN PRINCIPAL','Estado de Mexico','Aculco','15208','centro','Teoloyucan','878'),
+('ALM0002','ALMACEN SECUNDARIO','Ciudad de Mexico','Coyoacan','29534','centro','Liberta','711'),
+('ALM0003','ALMACEN PRINCIPAL','Ciudad de Mexico','Itztapalapa','47574','centro','El refugio','219'),
+('ALM0004','ALMACEN PRINCIPAL','Ciudad de Mexico','Alvaro Obregon','93148','centro','Omega','940')
+GO
+
+
---
+++ Datos para poblar tablas/Datos Sucursales inserciones.sql
@@ -0,0 +1,24 @@
+
+
+
+-----------------------------------DISTRIBUIDOR_O_SUCURSAL  -------------------------------------------------   SELECT * FROM DISTRIBUIDOR_O_SUCURSAL
+
+USE Nissan
+GO
+
+
+INSERT INTO DISTRIBUIDOR_O_SUCURSAL(ID_SUCURSAL,DESCRIPCION,ESTADO,CIUDAD,CP,COLONIA,CALLE,NUM_INT,PUNTUACION) 
+VALUES				
+('SUC00001','NISSAN GUANAJUATO','Guanajuato','Guanajuato','93100','centro'		,'20 de Enero'			,'611'	  ,0),
+('SUC00002','NISSAN IRAPUATO','Guanajuato','Irapuato','59392','centro'			,'Alamo'				,'549'	,0	),
+('SUC00003','NISSAN 1 LEON','Guanajuato','Leon','66743'	,'centro'				,'Agua Azul'			,'578'	,0			),
+('SUC00004','NISSAN 2 LEON','Guanajuato','Leon','66743'	,'centro'				,'San  Miguel'			,'500'	,0	),
+('SUC00005','SUC NISSAN DE SILAO','Guanajuato','Silao','84160','centro'		,'Alamo del valle'		,'429'		,0),
+('SUC00006','NISSAN YURIRIA','Guanajuato','Yuriria','25524'	,'centro'		,'Morelos'				,'780'		,0),
+('SUC00007','NISSAN SMA','Guanajuato','Ocampo','50783'	,'centro'			,'Madero'				,'102'		,0),
+('SUC00008','NISSAN LA PAZ','Estado de Mexico','Calimaya','14764','centro'	,'Valadez'	,'623'	,0),
+('SUC00009','NISSAN LERMA','Estado de Mexico','Coyotepec','50508'	,'centro','El dorado','649',0),
+('SUC000010','NISSAN 1 MORELOS','Estado de Mexico','Morelos','30897','centro','Lopez','487'	,0)
+
+GO
+
